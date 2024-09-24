902c886eed208f48f33aacbf6135e2b9434fc456
desh-dev <etahclinton506@gmail.com>
2024-09-24 02:16:55 +0100

    manual domain

--- app.ts
+++ app.ts
@@ -3,9 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-// import cors from "cors";
+import cors from "cors";
 import { NextFunction, Response, Request } from "express";
-// import allowedOrigins from "./config/allowedOrigins";
+import allowedOrigins from "./config/allowedOrigins";
 
 var collectPaymentRouter = require("./routes/collectPayment");
 var checkPaymentRouter = require("./routes/paymentStatus");
@@ -23,16 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const message = 'The CORS policy for this origin is not enabled.';
-//       return callback(new Error(message), false);
-//     }
-//     callback(null, true);
-//   }
-// }));
+app.use(cors({
+  origin: "https://checkgame.vercel.app/"
+}));
 app.use("/request-to-pay", collectPaymentRouter);
 app.use("/payment-status", checkPaymentRouter);
 app.use("/request-withdrawal", disbursePayment);
